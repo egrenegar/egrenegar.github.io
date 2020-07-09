0055877b1af6b3947b23b20203288703f4e8cda5
Emily Renegar <egrenegar@gmail.com>
2020-07-08 23:13:55 -0400

    Updates

---
+++ precache-manifest.b47ce9dc77594b12b82ce1d4a9b82ac3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e04abbb25590fa1ba21fcdccbb74e3a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ca4f74d97e69ac0b1fd",
+    "url": "/static/css/2.58c4053f.chunk.css"
+  },
+  {
+    "revision": "7f123a25472bc08c26e5",
+    "url": "/static/css/main.26391045.chunk.css"
+  },
+  {
+    "revision": "6ca4f74d97e69ac0b1fd",
+    "url": "/static/js/2.08d2909a.chunk.js"
+  },
+  {
+    "revision": "e01eb0f37af85dfcc8900424d5ca88ea",
+    "url": "/static/js/2.08d2909a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f123a25472bc08c26e5",
+    "url": "/static/js/main.4bf7c4ad.chunk.js"
+  },
+  {
+    "revision": "1cedc842a4d9f5a71dc7",
+    "url": "/static/js/runtime-main.f7c1c791.js"
+  }
+]);
